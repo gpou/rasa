40b8d35ca18fb83474ee6d333ab1504d4df92ca2
Tobias Wochinger <t.wochinger@rasa.com>
2021-07-07 11:08:27 +0200

    fix `return` value issues in `models` module

--- rasa/utils/tensorflow/model_data_utils.py
+++ rasa/utils/tensorflow/model_data_utils.py
@@ -141,7 +141,7 @@ def get_tag_ids(
 def _surface_attributes(
     features: List[List[Dict[Text, List["Features"]]]],
     featurizers: Optional[List[Text]] = None,
-) -> defaultdict[Text, List[List[Optional[List["Features"]]]]]:
+) -> Dict[Text, List[List[Optional[List["Features"]]]]]:
     """Restructure the input.
 
     "features" can, for example, be a dictionary of attributes (INTENT,
