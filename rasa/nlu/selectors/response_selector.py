50db4b1e6fcffcfbdc3d3e09756066c2c6d0e030
Joseph Juzl <joejuzl@gmail.com>
2021-07-07 09:31:51 +0200

    wip

--- rasa/nlu/selectors/response_selector.py
+++ rasa/nlu/selectors/response_selector.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import copy
 import logging
 
@@ -626,9 +627,9 @@ class ResponseSelector(DIETClassifier):
         meta: Dict[Text, Any],
         model_dir: Text,
         model_metadata: Metadata = None,
-        cached_component: Optional["ResponseSelector"] = None,
+        cached_component: Optional[ResponseSelector] = None,
         **kwargs: Any,
-    ) -> "ResponseSelector":
+    ) -> ResponseSelector:
         """Loads the trained model from the provided directory."""
 
         model = super().load(
